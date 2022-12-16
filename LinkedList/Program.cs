1bd1221d67d8e120a3bfd4aa909b5988790ee292
shubham07h <shubham1609sharma@gmail.com>
2022-12-16 23:06:41 +0530

    commit

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -5,14 +5,18 @@
 
         static void Main(string[] args)
         {
-            Linkedlist list = new Linkedlist();
-            list.Addnode(10);
-            list.Addnode(20);
-            list.Addnode(30);
-            list.Display();
-            list.DeleteLast();
-            list.Display();
-
+            // Linkedlist list = new Linkedlist();
+            // list.Addnode(10);
+            //list.Addnode(20);
+            //list.Addnode(30);
+            //list.Display();
+            // list.DeleteLast();
+            //list.Display();
+            Stack_Ex stack_ = new Stack_Ex();
+            stack_.Push(10);
+            stack_.Push(20);
+            stack_.Push(30);
+            stack_.Display();
 
 
         }
