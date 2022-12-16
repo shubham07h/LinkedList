a795dacdbb5425ea84f95e183d538f37405058bf
shubham07h <shubham1609sharma@gmail.com>
2022-12-16 20:59:01 +0530

    commit message

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class LinkedList
+    {
+        Node head;
+        public void Addnode(int data)
+        {
+            Node node = new Node(data);
+
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = node;
+            }
+            Console.WriteLine("{0} inserted into linked list", node.data);
+        }
+        public void Display()
+
+        {
+            if (head == null)
+            {
+                Console.WriteLine("List is empty");
+
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("List contains");
+                Console.ResetColor();
+                Node tempnode = head; ;
+                while (tempnode != null)
+                {
+                    Console.WriteLine(tempnode.data + " ");
+                    tempnode = tempnode.Next;
+                }
+                Console.WriteLine("\n");
+
+            }
+        }
+    }
+}
+    
+
---
+++ LinkedList/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LinkedList
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Addnode(10);
+            list.Addnode(20);
+            list.Addnode(30);
+            list.Display();
+        }
+    }
+}
